e43dcd503dac23ce104c61ce6921d4125cc76536
Vicky Nyffeler <vnyffeler@gmail.com>
2017-02-06 20:58:34 -0800

    added the form and made the submit button work on the new page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,15 @@ class BooksController < ApplicationController
 		@book = Book.new
 	end
 
+	def create
+		Book.create(book_params)
+		redirect_to root_path
+	end
+
+private
+
+def book_params
+	params.require(:book).permit(:title, :author, :year, :genre)
+end
+
 end
