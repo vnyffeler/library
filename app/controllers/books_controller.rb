dfd767d75f0783567a73aeffdf114b34813a0d59
Vicky Nyffeler <vnyffeler@gmail.com>
2017-02-09 22:37:20 -0800

    updated link in nav bar, added buttons on each book.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+	# before_action :set_book, only: [:show, :edit, :delete, :update]
 
 	def index 
 		@books = Book.all
@@ -9,9 +10,46 @@ class BooksController < ApplicationController
 	end
 
 	def create
-		Book.create(book_params)
-		redirect_to root_path
+		@book = Book.new(book_params)
+
+		if @book.save 
+			redirect_to root_path, notice: "Book was successfully added." 
+		else
+			render :new, alert: "Your book was not successfully saved."
+		end
 	end
+
+	def edit
+		# raise params
+
+		@book = Book.find_by(id: params[:id])
+	end
+
+
+	def update 
+		@book = Book.find_by(id: params[:id])
+
+		if @book
+			@book.update!(book_params)
+			redirect_to root_path, notice: "Book was successfully updated." 
+		else
+			render :edit, alert: "Your book was not successfully updated."
+		end
+	end
+
+	def destroy
+		@book = Book.find_by(id: params[:id])
+
+		if @book
+			@book.destroy
+			redirect_to root_path, notice: "Book was successfully deleted."
+
+		else
+			redirect_to root_path, alert: "Book was not successfully deleted."
+		end	
+	end
+
+
 
 private
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+	validates :title, presence: true
+	validates :author, presence: true
+	validates :year, presence: true
+	validates :genre, presence: true
 end
